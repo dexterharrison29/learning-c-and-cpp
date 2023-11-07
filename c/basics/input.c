096f8224001be88329c282467515cc6be7f3eb74
Dexter Harrison <147558735+dexterharrison29@users.noreply.github.com>
2023-11-07 14:30:28 -0500

    Create input.c

---
+++ c/basics/input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int errorCount;
+    printf("You are a programmer. How many headaches would you like today? ");
+    scanf("%d", &errorCount);
+    printf("%d\n", errorCount);
+    return 0;
+}
